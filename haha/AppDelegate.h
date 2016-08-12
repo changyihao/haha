81650bccad66448a7a456bff3d5124ee23f5e196
何凡 <echo34@192.168.7.108>
2016-08-12 18:44:43 +0800

    Initial Commit

---
+++ haha/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  haha
+//
+//  Created by echo34 on 16/8/12.
+//  Copyright © 2016年 echo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
