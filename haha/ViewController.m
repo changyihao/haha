0386f82b907fd5b5662e04a5623d2654ae83a5d8
何凡 <echo34@192.168.2.1>
2016-08-12 19:02:59 +0800

    加了qqqq

--- haha/ViewController.m
+++ haha/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"sssss");
     NSLog(@"ddddd");
+    NSLog(@"qqqq");
 }
 
 - (void)didReceiveMemoryWarning {
